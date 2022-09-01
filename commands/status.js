6f7b2e4d7d1fe27ccf2ff17e2c2ec98aa244108a
isakkr <isakkr@sbb.rs>
2022-09-01 23:36:56 +0200

    fixed status

--- commands/status.js
+++ commands/status.js
@@ -1,12 +1,11 @@
 const { version } = require("discord.js");
 const moment = require("moment");
-const { EmbedBuilder } = require('discord.js');
 require("moment-duration-format");
 
 exports.run = (client, message) => {
     const dur = moment.duration(client.uptime).format(" D [days], H [hrs], m [mins], s [secs]");
     const mem = (process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)
-    const usr = client.users.cache.size
+    const usr = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)
     const ser = client.guilds.cache.size
     const chn = client.channels.cache.size
     const ver = version
@@ -18,13 +17,15 @@ exports.run = (client, message) => {
     description += "|     Channels :: " + chn + "\n";
     description += "|   Discord.js :: " + "v" + ver + "\n";
     description += "\`\`\`";
-   
-    const exampleEmbed = new EmbedBuilder()
-	.setColor(2515024)
-	.setTitle('☢ My statistics!')
-	.setDescription(description)
-	.setTimestamp()
-	.setFooter({ text: 'Bran from Isak ⚠'});
 
-   message.channel.send({ embeds: [exampleEmbed] });
+    const status ={
+        color: 2515024, 
+        title: "☢ My statistics!",
+        description : description,
+        timestamp: new Date().toISOString(),
+        footer: {
+            text: "Bran from Isak ⚠"
+        }
+    };
+    message.channel.send({ embeds: [status]});
 }
